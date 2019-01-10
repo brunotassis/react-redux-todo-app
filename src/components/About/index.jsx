892bcb7ebde6bb7a4e1927dcd302c446dfa3f8df
Bruno Assis de Tassis <brunotassis@gmail.com>
2019-01-10 20:35:47 -0300

    Complementação do component About.

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -4,5 +4,12 @@ import PageHeader from '../Template/pageHeader';
 export default props => (
     <div>
         <PageHeader name="Sobre" small="Nós" />
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Impresa</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
     </div>
 );
