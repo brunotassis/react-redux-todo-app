1411a8e5a0713643e3e80ac1f076d2366ea62e85
Bruno Assis de Tassis <brunotassis@gmail.com>
2019-01-13 11:32:35 -0300

    Adição das keys Enter e Esc para Adicionar, Buscar e Limpar dados no imput.

--- src/components/Todo/todoForm.jsx
+++ src/components/Todo/todoForm.jsx
@@ -2,23 +2,34 @@ import React from 'react';
 import Grid from '../Template/grid';
 import IconButton from '../Template/iconButton';
 
-export default props => (
-    <div role='form' className='todoForm row'>
-        <Grid cols="12 9 10">
-            <input type="text"
-                id="description"
-                className="form-control"
-                onChange={props.handleChange}
-                value={props.description}/>
-        </Grid>
+export default props => {
+    const keyHandler = (e) => {
+        if(e.key === 'Enter'){
+            e.shiftKey ? props.handleSearch() : props.handleAdd();
+        }else if(e.key === 'Escape'){
+            props.handleClear();
+        }
+    }
 
-        <Grid cols="12 3 2">
-            <IconButton styles='primary'
-                icon='plus' onClick={props.handleAdd} />
-            <IconButton styles='info'
-                icon='search' onClick={props.handleSearch} />
-            <IconButton styles='default'
-                icon='close' onClick={props.handleClear} />
-        </Grid>
-    </div>
-);
+    return (
+        <div role='form' className='todoForm row'>
+            <Grid cols="12 9 10">
+                <input type="text"
+                    id="description"
+                    className="form-control"
+                    onKeyUp={keyHandler}
+                    onChange={props.handleChange}
+                    value={props.description} />
+            </Grid>
+
+            <Grid cols="12 3 2">
+                <IconButton styles='primary'
+                    icon='plus' onClick={props.handleAdd} />
+                <IconButton styles='info'
+                    icon='search' onClick={props.handleSearch} />
+                <IconButton styles='default'
+                    icon='close' onClick={props.handleClear} />
+            </Grid>
+        </div>
+    )
+};
